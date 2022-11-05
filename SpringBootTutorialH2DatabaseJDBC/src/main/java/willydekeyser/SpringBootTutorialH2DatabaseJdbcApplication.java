fa937c9c1807dfa1eb0883f571432fce14d8ccb4
willydekeyser <willy.de.keyser@skynet.be>
2022-11-05 11:44:02 +0100

    Setup project

---
+++ SpringBootTutorialH2DatabaseJDBC/src/main/java/willydekeyser/SpringBootTutorialH2DatabaseJdbcApplication.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialH2DatabaseJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialH2DatabaseJdbcApplication.class, args);
+	}
+
+}
