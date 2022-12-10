be74729d65d173ea5efb9765a776b78ea18d0141
willydekeyser <willy.de.keyser@skynet.be>
2022-12-10 12:14:11 +0100

    Update Repository

--- SpringBootTutorialH2DatabaseJDBC/src/main/java/willydekeyser/repository/UserRepository.java
+++ SpringBootTutorialH2DatabaseJDBC/src/main/java/willydekeyser/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package willydekeyser.repository;
 
 import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.stereotype.Repository;
 
 import willydekeyser.model.Users;
 
-@Repository
 public interface UserRepository extends ListCrudRepository<Users, Integer> {
 
 }
